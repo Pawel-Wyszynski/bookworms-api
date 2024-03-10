b61b82e82f6a3ba8f6393740e0303b260c1fdf89
Dagmara Skulimowska <104433434+dagmaraskulimowska@users.noreply.github.com>
2024-03-10 22:00:45 +0100

    added-email-send

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactUsMail;
+
+class EmailController extends Controller
+{
+    public function sendEmail(Request $request) {
+        $emailAddress = env('MAIL_USERNAME');
+
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        Mail::to($emailAddress)->send(new ContactUsMail(($validatedData)));
+    }
+}
---
+++ resources/views/mail/contactUsMail.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Form Email</title>
+</head>
+
+<body>
+    <h2>Contact Form Email</h2>
+    <p>Username: {{ $data['name'] }}</p>
+    <p>Email: {{ $data['email'] }}</p>
+    <p>Message: {{ $data['message'] }}</p>
+</body>
+
+</html>
