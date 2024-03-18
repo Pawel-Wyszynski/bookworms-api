834217d08259653723e4db546e9d698af25f3984
Dagmara Skulimowska <104433434+dagmaraskulimowska@users.noreply.github.com>
2024-03-18 12:59:11 +0100

    added-change-password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -56,7 +58,22 @@ class UserController extends Controller
 
 public function changePassword(Request $request, $id)
 {
-   
+    $user = User::find($id);
+
+    if (!Hash::check($request->oldPassword, $user->password)) {
+        return response()->json(['message' => 'Invalid password'], 400);
+    }
+
+    if ($request->newPassword == $request->confirmPassword) {
+
+    $user->update([
+        'password' => Hash::make($request->newPassword),
+     ]);
+    } 
+    else  
+    return response()->json(['message' => 'Invalid password confirmation'], 400);
+
+    return response()->json($user);
 }
 
 
